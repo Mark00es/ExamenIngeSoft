3505a011c151b28d7ff15c554779707885ce0956
Mark04 <marco.escobar@ucb.edu.bo>
2022-05-31 11:54:58 -0400

    GuardaTodoValorInicial

--- src/cambioMonedas.test.js
+++ src/cambioMonedas.test.js
@@ -3,4 +3,7 @@ describe("Restar", () => {
     it("Si el monto es 19 Bs debe mostrar ese valor", () => {
       expect(cambioMonedas(19)).toEqual(19);
     });    
+    it("Si el monto es 32 Bs debe mostrar ese valor", () => {
+        expect(cambioMonedas(32)).toEqual(32);
+      });    
   });
