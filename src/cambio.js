8e8dcd530474aa095878d1d418f6c4b219d317ae
Mark04 <marco.escobar@ucb.edu.bo>
2022-05-31 12:08:33 -0400

    implementandoVector2Cambios

--- src/cambio.js
+++ src/cambio.js
@@ -6,9 +6,14 @@ class cambio{
         return this.valor
     }        
     cambioMonedas(){
-        let vectorResultante = []
+        let vectorResultante = []        
         for(let cambio = 0;cambio<=this.valor;cambio = cambio + cambio){
-            cambio = this.valor;
+            if(this.valor%2 == 0){
+                cambio = 2;
+            }
+            if(this.valor%5 == 0){
+                cambio = 5;
+            }
             vectorResultante.push(cambio)            
         }        
         return vectorResultante        
